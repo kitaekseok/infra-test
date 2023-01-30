de76ab8beddfe17e7e1be3df402cf6f4a84967df
투라인코드-석기택 <kitaek.seok@twolinecode.com>
2023-01-30 14:08:27 +0900

    create spring boot project

---
+++ src/main/java/com/example/infratest/InfraTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.infratest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfraTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfraTestApplication.class, args);
+	}
+
+}
